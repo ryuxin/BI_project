b15690a31f5050d269c544c31aff1a7aa645307d
ryuxin <m.phelps.a@gmail.com>
2018-12-03 22:52:54 -0500

    remove unused code

--- mc_code/2pc_mc/share/constant.h
+++ mc_code/2pc_mc/share/constant.h
@@ -13,11 +13,6 @@
 #define HASHPOWER_DEFAULT         22
 #define hashsize(n)               ((unsigned long int)1<<(n))
 #define hashmask(n)               (hashsize(n)-1)
-#define round_to_pow2(x, pow2)    (((unsigned long)(x))&(~((pow2)-1)))
-#define round_up_to_pow2(x, pow2) (round_to_pow2(((unsigned long)x)+(pow2)-1, (pow2)))
-#define round_to_cacheline(x)     round_to_pow2(x, CACHE_LINE)
-#define round_to_page(x)          round_to_pow2(x, PAGE_SIZE)
-#define round_up_to_page(x)       round_up_to_pow2(x, PAGE_SIZE)
 /* #define KEY_SKEW                  1 */
 #define EVICT_NUM                 20
 
