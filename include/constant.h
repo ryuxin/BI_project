5cf9b0201c18bc1a9e75b2d7a979bcd66ce8885f
ryuxin <m.phelps.a@gmail.com>
2019-04-16 14:43:08 -0500

    util: mv some util to hw_util

--- include/constant.h
+++ include/constant.h
@@ -34,16 +34,4 @@
 /******** TESTS ************/
 #define MAX_TEST_OBJ_NUM (1024)
 
-#ifndef likely
-#define likely(x)      __builtin_expect(!!(x), 1)
-#endif
-#ifndef unlikely
-#define unlikely(x)    __builtin_expect(!!(x), 0)
-#endif
-#define round_to_pow2(x, pow2)    (((unsigned long)(x))&(~((pow2)-1)))
-#define round_up_to_pow2(x, pow2) (round_to_pow2(((unsigned long)x)+(pow2)-1, (pow2)))
-#define round_to_cacheline(x)     round_to_pow2(x, CACHE_LINE)
-#define round_to_page(x)          round_to_pow2(x, PAGE_SIZE)
-#define round_up_to_page(x)       round_up_to_pow2(x, PAGE_SIZE)
-
 #endif /* CONSTANT_H */
--- include/hw_util.h
+++ include/hw_util.h
@@ -12,6 +12,18 @@ extern int num_node_in_use;
 extern int num_core_in_use;
 extern __thread int local_core_id;
 
+#ifndef likely
+#define likely(x)      __builtin_expect(!!(x), 1)
+#endif
+#ifndef unlikely
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+#endif
+#define round_to_pow2(x, pow2)    (((unsigned long)(x))&(~((pow2)-1)))
+#define round_up_to_pow2(x, pow2) (round_to_pow2(((unsigned long)x)+(pow2)-1, (pow2)))
+#define round_to_cacheline(x)     round_to_pow2(x, CACHE_LINE)
+#define round_to_page(x)          round_to_pow2(x, PAGE_SIZE)
+#define round_up_to_page(x)       round_up_to_pow2(x, PAGE_SIZE)
+
 #define NODE_ID() (local_node_id)
 #define CORE_ID() (local_core_id)
 
